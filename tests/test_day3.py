cb90da02110ca45134ea40732109dec545b59883
pmarflee <paul.marfleet@gmail.com>
2022-12-04 17:45:04 +0000

    Day 3 - fixed unit test names

--- tests/test_day3.py
+++ tests/test_day3.py
@@ -8,8 +8,8 @@ class TestDay3:
               'ttgJtRGJQctTZtZT',
               'CrZsJsPPZsGzwwsLwLmpwMDw']
 
-    def test_day2_calculate_part1(self):
+    def test_day3_calculate_part1(self):
         assert day3.calculate(self.lines, 1) == 157, 'incorrect value'
 
-    def test_day2_calculate_part2(self):
+    def test_day3_calculate_part2(self):
         assert day3.calculate(self.lines, 2) == 70, 'incorrect value'
