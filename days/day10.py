18c0e367a31ec79318515d966fff873bc52c866f
pmarflee <paul.marfleet@gmail.com>
2023-03-25 18:01:39 +0000

    Day 10 - Created separate class for Clock Circuit. Moved Parser into main module.

--- days/day10.py
+++ days/day10.py
@@ -1,12 +1,6 @@
 from typing import List
 from abc import ABC, abstractmethod
 
-def calculate(lines: List[str], part: int):
-    cpu = CPU()
-    for line in lines:
-        cpu.execute(line)
-    return cpu.sum_signal_strengths
-
 class Instruction:
     def __init__(self, cycles):
         self.cycles = cycles
@@ -20,31 +14,42 @@ class AddX(Instruction):
         super().__init__(2)
         self.value = value
 
+class ClockCircuit:
+    def __init__(self) -> None:
+        self.cycles = 0
+    
+    def tick(self) -> None:
+        self.cycles += 1
+
 class CPU:
     sample_cycles = [20, 60, 100, 140, 180, 220]
 
-    def __init__(self):
+    def __init__(self, clock_circuit: ClockCircuit) -> None:
         self.X = 1
-        self.__cycles = 0
+        self.__clock_circuit = clock_circuit
         self.__signal_strengths = []
 
     @property
     def sum_signal_strengths(self):
         return sum(self.__signal_strengths)
 
-    def execute(self, line: str):
-        instruction = CPU.__parse_instruction(line)
+    def execute(self, instruction: Instruction):
         for _ in range(instruction.cycles):
-            self.__cycles += 1
-            if self.__cycles in CPU.sample_cycles:
-                self.__signal_strengths.append(self.__cycles * self.X)
+            self.__clock_circuit.tick()
+            if self.__clock_circuit.cycles in CPU.sample_cycles:
+                self.__signal_strengths.append(self.__clock_circuit.cycles * self.X)
         if isinstance(instruction, AddX):
             self.X += instruction.value
 
-    @staticmethod
-    def __parse_instruction(line: str) -> Instruction:
-        if line == 'noop':
-            return Noop()
-        parts = line.split(' ')
-        return AddX(int(parts[1]))
+def calculate(lines: List[str], part: int):
+    cpu = CPU(ClockCircuit())
+    for line in lines:
+        instruction = __parse(line)
+        cpu.execute(instruction)
+    return cpu.sum_signal_strengths
 
+def __parse(line: str) -> Instruction:
+    if line == 'noop':
+        return Noop()
+    parts = line.split(' ')
+    return AddX(int(parts[1]))
