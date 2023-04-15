a4909b7609d7d1f0d8e066e8dc109bed19f63c5d
pmarflee <paul.marfleet@gmail.com>
2023-04-15 23:11:20 +0100

    Day 14 - renamed parse_lines to parse_paths Return min_x, max_x, and max_y values for paths

--- days/day14.py
+++ days/day14.py
@@ -1,13 +1,24 @@
 def calculate(lines, part):
     pass
 
-def parse_lines(lines):
+def parse_paths(lines):
     paths = []
+    min = 0
+    max = 0
+    max_y = 0
     for line in lines:
         path = []
         items = line.split('->')
         for item in items:
             left, _, right = item.strip().partition(',')
-            path.append((int(left), int(right)))
+            int_left = int(left)
+            if min == 0 or min > int_left:
+                min = int_left
+            if max == 0 or max < int_left:
+                max = int_left
+            int_right = int(right)
+            if max_y == 0 or max_y < int_right:
+                max_y = int_right
+            path.append((int_left, int_right))
         paths.append(path)
-    return paths
+    return paths, min, max, max_y
