550403cb74ec1a6e049918271d205dae9fc12f0c
pmarflee <paul.marfleet@gmail.com>
2023-04-15 22:45:12 +0100

    Day 14 - implemented parse_lines

--- days/day14.py
+++ days/day14.py
@@ -1,2 +1,13 @@
 def calculate(lines, part):
     pass
+
+def parse_lines(lines):
+    paths = []
+    for line in lines:
+        path = []
+        items = line.split('->')
+        for item in items:
+            left, _, right = item.strip().partition(',')
+            path.append((int(left), int(right)))
+        paths.append(path)
+    return paths
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -1,4 +1,11 @@
 from days import day14
 
 class TestDay14:
-    pass
+    lines = ['498,4 -> 498,6 -> 496,6',
+             '503,4 -> 502,4 -> 502,9 -> 494,9']
+    
+    parse_expected = [ [ (498,4), (498,6), (496,6 ) ],
+                       [ (503,4), (502,4), (502,9), (494,9 ) ] ]
+    
+    def test_day14_parse_lines(self):
+        assert day14.parse_lines(self.lines) == self.parse_expected
