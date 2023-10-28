a566379d5b90f3b9586fff950192c4b1c375c197
pmarflee <paul.marfleet@gmail.com>
2023-10-28 18:03:51 +0100

    Day 14, Part 1: implemented drawing cave

--- days/day14.py
+++ days/day14.py
@@ -1,10 +1,63 @@
+from dataclasses import dataclass
+from enum import Enum
+from itertools import pairwise
+from typing import Optional
+
+
+@dataclass
+class ScanResult:
+    paths: list[list[tuple[int, int]]]
+    min_x: int
+    max_x: int
+    max_y: int
+
+
+class Stuff(Enum):
+    ROCK = 1
+    SAND = 2
+
+
+class Cave:
+    def __init__(self, scan: ScanResult):
+        self.__offset = scan.min_x
+        self.source = (0, 500)
+
+        width = scan.max_x - scan.min_x
+        data: list[list[Optional[Stuff]]] = [[None for _ in range(width + 1)] for _ in range(scan.max_y + 1)]
+
+        for line in scan.paths:
+            for (x_previous, y_previous), (x_current, y_current) in pairwise(line):
+                for x, y in self.__get_rock_range(x_previous, y_previous, x_current, y_current):
+                    data[y][x] = Stuff.ROCK
+
+        self.__width = width
+        self.__data = data
+
+    @property
+    def data(self):
+        return self.__data
+
+    def __get_row_index(self, x: int) -> int:
+        return x - self.__offset
+
+    def __get_rock_range(self, x_previous: int, y_previous: int, x_current: int, y_current: int) -> list[tuple[int, int]]:
+        if x_previous == x_current:
+            x = self.__get_row_index(x_previous)
+            min_y, max_y = min(y_previous, y_current), max(y_previous, y_current)
+            return [(x, y) for y in range(min_y, max_y + 1)]
+        else:
+            min_x, max_x = min(x_previous, x_current), max(x_previous, x_current)
+            return [(self.__get_row_index(x), y_previous) for x in range(min_x, max_x + 1)]
+
+
 def calculate(lines, part):
     pass
 
+
 def parse_paths(lines):
     paths = []
-    min = 0
-    max = 0
+    min_x = 0
+    max_x = 0
     max_y = 0
     for line in lines:
         path = []
@@ -12,13 +65,13 @@ def parse_paths(lines):
         for item in items:
             left, _, right = item.strip().partition(',')
             int_left = int(left)
-            if min == 0 or min > int_left:
-                min = int_left
-            if max == 0 or max < int_left:
-                max = int_left
+            if min_x == 0 or min_x > int_left:
+                min_x = int_left
+            if max_x == 0 or max_x < int_left:
+                max_x = int_left
             int_right = int(right)
             if max_y == 0 or max_y < int_right:
                 max_y = int_right
             path.append((int_left, int_right))
         paths.append(path)
-    return paths, min, max, max_y
+    return ScanResult(paths=paths, min_x=min_x, max_x=max_x, max_y=max_y)
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -1,11 +1,30 @@
 from days import day14
+from days.day14 import Stuff
 
 class TestDay14:
     lines = ['498,4 -> 498,6 -> 496,6',
              '503,4 -> 502,4 -> 502,9 -> 494,9']
-    
-    parse_expected = [ [ (498,4), (498,6), (496,6 ) ],
-                       [ (503,4), (502,4), (502,9), (494,9 ) ] ]
-    
+
+    scan_result = day14.ScanResult(
+        paths=[[(498, 4), (498, 6), (496, 6)],
+         [(503, 4), (502, 4), (502, 9), (494, 9)]],
+        min_x=494, max_x=503, max_y=9)
+
+    cave_data = [
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, Stuff.ROCK, None, None, None, Stuff.ROCK, Stuff.ROCK],
+        [None, None, None, None, Stuff.ROCK, None, None, None, Stuff.ROCK, None],
+        [None, None, Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, None, None, None, Stuff.ROCK, None],
+        [None, None, None, None, None, None, None, None, Stuff.ROCK, None],
+        [None, None, None, None, None, None, None, None, Stuff.ROCK, None],
+        [Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, Stuff.ROCK, None]
+    ]
+
     def test_day14_parse_lines(self):
-        assert day14.parse_lines(self.lines) == self.parse_expected
+        assert day14.parse_paths(self.lines) == self.scan_result
+
+    def test_day14_draw_cave(self):
+        assert day14.Cave(self.scan_result).data == self.cave_data
